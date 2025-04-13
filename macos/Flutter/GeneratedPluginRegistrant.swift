8c35798f4e0edbdcda3ecda06118656994315ccb
Hugo Alberto Garcia <hugo.a.garcia@gmail.com>
2025-04-13 15:43:23 +0200

    Files used for Linux native apps.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,7 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_blue_plus
+import flutter_blue_plus_darwin
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
